94494c650960ef50d2c1babacb785c81042ba9db
GitHub Action <action@github.com>
2020-05-23 21:28:45 +0000

    Add build changes

---
+++ precache-manifest.dcc859b8991ea8f3233bce90cf91f99d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e7d2f7594b57e1ae279fb6bf396b0a41",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3bdf3858f6b74adcdac5",
+    "url": "/static/css/main.5b2dae89.chunk.css"
+  },
+  {
+    "revision": "0caa7dc7078bf0bbebe7",
+    "url": "/static/js/2.c2987b2d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.c2987b2d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3bdf3858f6b74adcdac5",
+    "url": "/static/js/main.8c5e765d.chunk.js"
+  },
+  {
+    "revision": "7edcd2791d48a411c95a",
+    "url": "/static/js/runtime-main.c27c0083.js"
+  },
+  {
+    "revision": "9f2195308d1685b3d1fb61eb526e7e00",
+    "url": "/static/media/tree.9f219530.svg"
+  }
+]);
